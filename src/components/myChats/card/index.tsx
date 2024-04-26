f6277854ad6efa82308d514837aea9a98a3769de
Erickdevup <erickgarciap779@gmail.com>
2024-04-26 23:43:55 +0200

    my chats cards responsive and handle selected with url done

--- src/components/myChats/card/index.tsx
+++ src/components/myChats/card/index.tsx
@@ -1,21 +1,27 @@
+"use client";
 import Chat from "@/app/icons";
+import ChatCardLayout from "@/components/helpers/chatCardLayout";
+import { GetChatID } from "@/hooks/getChatID";
 import React from "react";
 
-type Props = {isSelected?:boolean};
+type Props = { id?: string };
 
-export default function ChatsCard({isSelected=false}: Props) {
+export default function ChatsCard({ id }: Props) {
+  const chatId = GetChatID();
+
   return (
-    <div className={`flex w-full gap-2 px-2 py-3 ${isSelected && "bg-core-bg "} rounded-xl`}>
+    <ChatCardLayout id={id}>
       <Chat />
       <div className="w-full">
         <h3 className="text-sm text-core-text font-semibold overflow-hidden whitespace-nowrap text-ellipsis max-w-[80%]">
           Warning Messages Samples
         </h3>
         <p className="text-core-secText text-sm w-full line-clamp-2">
-            Lorem consectetur adipisicing elit. Tempora consequa rcitationem! Lorem ipsum dolor sit amet.
+          Lorem consectetur adipisicing elit. Tempora consequa rcitationem!
+          Lorem ipsum dolor sit amet.
         </p>
       </div>
       <span className="text-xs ml-auto text-core-secText">New</span>
-    </div>
+    </ChatCardLayout>
   );
 }
